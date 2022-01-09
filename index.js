d485bcaef38dfce0d8596b4ffbe14b601415fb94
Julia <yosoyjulia@gmail.com>
2022-01-09 14:05:26 -0300

    subiendo el TP

---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let app = express();
+const PORT = 3000;
+let path = require('path');
+
+// Middlewares
+app.use(express.static("public"));
+
+// Routes
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, './views/home.html'))
+});
+
+app.get('/register', function (req, res){
+    res.sendFile(path.join(__dirname, './views/register.html'))
+});
+
+app.get('/login', function (req, res){
+    res.sendFile(path.join(__dirname, './views/login.html'))
+});
+
+// Server
+app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}
+http://localhost:${PORT}`));
